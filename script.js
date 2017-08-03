1da24955adc205af2bd470edb3a4125a442c51c0
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2017-08-03 16:10:21 +0200

    More...

---
+++ script.js
@@ -0,0 +1,18 @@
+function fn() {
+    var button = document.getElementById("button"),
+        image = document.getElementById("pista3"),
+        pista = document.getElementById("pista3text"),
+        input = document.getElementById("input");
+
+    button.addEventListener("click", function(){
+        if (input.value == "45") {
+            image.style.display = "block";
+            pista.style.display = "block";
+        } else {
+            alert("algo está mal");
+        }
+    }, false);
+}
+document.addEventListener('DOMContentLoaded', fn, false);
+
+
